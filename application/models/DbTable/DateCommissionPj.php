d637e4e66589352fcd71718911457f6b40865457
CFX-SDIS33 <christophe.friloux@cegetel.net>
2018-10-08 15:53:05 +0200

    - Correction de régressions suite aux développements 2018 :
       + La liste des dossiers prévus dans l'ordre du jour de la sous-com
    (écran "Calendrier > Commission départementale > Afficher l'ordre du
    jour") n'était plus triée automatiquement par ordre alphabétique des
    noms de communes liées au dossier puis par ordre alphabétique des noms
    d'ERP
       + Les dossiers liés à des cellules (ERP du genre cellule)
    n'apparaissaient plus dans les éditions relatives à l'ordre du jour de
    la sous-com (convocations, ordre du jour, compte rendu)

--- application/models/DbTable/DateCommissionPj.php
+++ application/models/DbTable/DateCommissionPj.php
@@ -73,27 +73,45 @@ class Model_DbTable_DateCommissionPj extends Zend_Db_Table_Abstract
     public function getDossiersInfosByOrder($dateCommId)
     {
 		//Retourne les dossiers avec toutes les informations le concernant class�s par ordre
-		$select = $this->select()
+		$selectEtab = $this->select()
 			->setIntegrityCheck(false)
 			->from(array('doss' => 'dossier'))
 			->join(array('dossAffect' => 'dossieraffectation'),'doss.ID_DOSSIER = dossAffect.ID_DOSSIER_AFFECT')
 			->join(array('dateComm' => 'datecommission'),'dossAffect.ID_DATECOMMISSION_AFFECT = dateComm.ID_DATECOMMISSION')
-			->join(array('etabDoss' => 'etablissementdossier'),'etabDoss.ID_DOSSIER = doss.ID_DOSSIER')
-			->join(array('etabInfos' => 'etablissementinformations'),'etabInfos.ID_ETABLISSEMENT = etabDoss.ID_ETABLISSEMENT AND etabInfos.DATE_ETABLISSEMENTINFORMATIONS = ( SELECT MAX(DATE_ETABLISSEMENTINFORMATIONS) FROM  etablissementinformations WHERE ID_ETABLISSEMENT = etabDoss.ID_ETABLISSEMENT)', "LIBELLE_ETABLISSEMENTINFORMATIONS")
-			->join(array('etabAdr' => 'etablissementadresse'),'etabDoss.ID_ETABLISSEMENT = etabAdr.ID_ETABLISSEMENT')
-			->join(array('adrComm' => 'adressecommune'),'etabAdr.NUMINSEE_COMMUNE = adrComm.NUMINSEE_COMMUNE', "LIBELLE_COMMUNE")
+			->joinLeft(array('etabDoss' => 'etablissementdossier'),'etabDoss.ID_DOSSIER = doss.ID_DOSSIER')
+			->joinLeft(array('etabInfos' => 'etablissementinformations'),'etabInfos.ID_ETABLISSEMENT = etabDoss.ID_ETABLISSEMENT AND etabInfos.DATE_ETABLISSEMENTINFORMATIONS = ( SELECT MAX(DATE_ETABLISSEMENTINFORMATIONS) FROM  etablissementinformations WHERE ID_ETABLISSEMENT = etabDoss.ID_ETABLISSEMENT)', "LIBELLE_ETABLISSEMENTINFORMATIONS AS LIB_ETABLISSEMENT")
+			->joinLeft(array('etabAdr' => 'etablissementadresse'),'etabDoss.ID_ETABLISSEMENT = etabAdr.ID_ETABLISSEMENT')
+			->joinLeft(array('adrComm' => 'adressecommune'),'etabAdr.NUMINSEE_COMMUNE = adrComm.NUMINSEE_COMMUNE', "LIBELLE_COMMUNE AS LIB_COMMUNE")
 			->join(array('dossNat' => 'dossiernature'),'dossNat.ID_DOSSIER = doss.ID_DOSSIER')
 			->join(array('dossNatListe' => 'dossiernatureliste'),'dossNat.ID_NATURE = dossNatListe.ID_DOSSIERNATURE')
-			//->joinLeft(array('docurba' => 'dossierdocurba'),'docurba.ID_DOSSIER = doss.ID_DOSSIER')
 			->where('dateComm.ID_DATECOMMISSION = ?',$dateCommId)
-			->group('doss.ID_DOSSIER')
-// 			->order('dossAffect.NUM_DOSSIER');
-			->order("adrComm.LIBELLE_COMMUNE")
-			->order("etabInfos.LIBELLE_ETABLISSEMENTINFORMATIONS");
+			->where("adrComm.LIBELLE_COMMUNE IS NOT NULL")
+			->group('doss.ID_DOSSIER');
+		
+		$selectCellule = $this->select()
+			->setIntegrityCheck(false)
+			->from(array('doss' => 'dossier'))
+			->join(array('dossAffect' => 'dossieraffectation'),'doss.ID_DOSSIER = dossAffect.ID_DOSSIER_AFFECT')
+			->join(array('dateComm' => 'datecommission'),'dossAffect.ID_DATECOMMISSION_AFFECT = dateComm.ID_DATECOMMISSION')
+			->joinLeft(array('etabDoss' => 'etablissementdossier'),'etabDoss.ID_DOSSIER = doss.ID_DOSSIER')
+			->joinLeft(array('etabInfos' => 'etablissementinformations'),'etabInfos.ID_ETABLISSEMENT = etabDoss.ID_ETABLISSEMENT AND etabInfos.DATE_ETABLISSEMENTINFORMATIONS = ( SELECT MAX(DATE_ETABLISSEMENTINFORMATIONS) FROM  etablissementinformations WHERE ID_ETABLISSEMENT = etabDoss.ID_ETABLISSEMENT)', "LIBELLE_ETABLISSEMENTINFORMATIONS AS LIB_ETABLISSEMENT")
+			->joinLeft("etablissementlie", "etabInfos.ID_ETABLISSEMENT = etablissementlie.ID_FILS_ETABLISSEMENT", null)
+			->joinLeft(array("etabAdrCellule" => "etablissementadresse"), "etabAdrCellule.ID_ETABLISSEMENT = (SELECT ID_ETABLISSEMENT FROM etablissementlie WHERE ID_FILS_ETABLISSEMENT = etabInfos.ID_ETABLISSEMENT LIMIT 1)")
+			->joinLeft(array("adrCommCellule" => "adressecommune"), "etabAdrCellule.NUMINSEE_COMMUNE = adrCommCellule.NUMINSEE_COMMUNE", "LIBELLE_COMMUNE AS LIB_COMMUNE")
+			->join(array('dossNat' => 'dossiernature'),'dossNat.ID_DOSSIER = doss.ID_DOSSIER')
+			->join(array('dossNatListe' => 'dossiernatureliste'),'dossNat.ID_NATURE = dossNatListe.ID_DOSSIERNATURE')
+			->where('dateComm.ID_DATECOMMISSION = ?',$dateCommId)
+			->where("adrCommCellule.LIBELLE_COMMUNE IS NOT NULL")
+			->group('doss.ID_DOSSIER');
 		
 		//echo $select->__toString();
 		
-        return $this->getAdapter()->fetchAll($select);
+			$return = $this->fetchAll($this->select()->union(array($selectEtab,	$selectCellule))
+					->order("LIB_COMMUNE ASC")
+					->order("LIB_ETABLISSEMENT ASC")
+					)->toArray();
+		
+			return $return;
     }
 
 	public function getPjInfos($idComm)
